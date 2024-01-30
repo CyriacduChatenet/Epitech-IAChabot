42988d882b6770835ca67494ebbc1461855bc33a
CyriacduChatenet <cyriacduchatenet@icloud.com>
2024-01-30 15:33:33 +0100

    fix(api): add CORS

--- api/api.py
+++ api/api.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 
 # Route de base
 @app.route('/api/hello', methods=['GET'])
