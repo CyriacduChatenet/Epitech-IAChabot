a62fafad4a03f6d0eb2beb31de4ab21dadefaae2
CyriacduChatenet <cyriacduchatenet@icloud.com>
2024-01-30 11:34:53 +0100

    feat(api): add langchain chatbot feature

---
+++ api/main.py
@@ -0,0 +1,57 @@
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.vectorstores import FAISS
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.documents import Document
+from langchain.chains import create_retrieval_chain
+
+chat = True
+
+def invokeChatbot (user_input) :
+    prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a journalist."),
+    ("user", "{input}")
+    ])
+    loader = WebBaseLoader("https://www.bbc.com/sport/football/european-championship/scores-fixtures/2023-11")
+    docs = loader.load()
+    
+    llm = ChatOpenAI(openai_api_key="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")
+    embeddings = OpenAIEmbeddings(openai_api_key="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")
+    output_parser = StrOutputParser()
+    text_splitter = RecursiveCharacterTextSplitter()
+    documents = text_splitter.split_documents(docs)
+    vector = FAISS.from_documents(documents, embeddings)
+    
+    chain = prompt | llm | output_parser
+    
+    prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
+    
+    <context>
+    {context}
+    </context>
+    
+    Question: {input}""")
+    
+    document_chain = create_stuff_documents_chain(llm, prompt)
+    
+    document_chain.invoke({
+        "input": user_input,
+        "context": [Document(page_content="tell us all matches result")]
+    })
+    
+    retriever = vector.as_retriever()
+    retrieval_chain = create_retrieval_chain(retriever, document_chain)
+    
+    response = retrieval_chain.invoke({"input": user_input})
+    print(response["answer"])
+
+while chat == True :
+    user_input = input("Enter your question: ")
+    if user_input == "exit":
+        chat = False
+    else:
+        invokeChatbot(user_input)
