344dad893cb752f15269b0b6b9810e3565625953
CyriacduChatenet <cyriacduchatenet@icloud.com>
2024-01-31 12:09:11 +0100

    feat(llm): add llm memory

--- test.py
+++ test.py
@@ -12,10 +12,21 @@ from langchain_community.llms import Ollama
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 
+class ChatbotMemory:
+    def __init__(self):
+        self.previous_responses = []
+
+    def add_response(self, response):
+        self.previous_responses.append(response)
+
+    def get_previous_responses(self):
+        return self.previous_responses
+
 prompt = ChatPromptTemplate.from_messages([
     ("system", "You are a journalist."),
     ("user", "{input}")
 ])
+
 loader = WebBaseLoader("https://www.lequipe.fr/Rugby/top-14/page-calendrier-resultats/13e-journee")
 docs = loader.load()
 
@@ -26,7 +37,7 @@ text_splitter = RecursiveCharacterTextSplitter()
 documents = text_splitter.split_documents(docs)
 vector = FAISS.from_documents(documents, embeddings) 
 
-#chain = LLMChain(prompt=prompt, llm=llm)
+memory = ChatbotMemory()
 
 prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
 
@@ -39,12 +50,20 @@ Question: {input}""")
 document_chain = create_stuff_documents_chain(llm, prompt)
 
 document_chain.invoke({
-    "input": "Tu peux me donner des score de match du top14?",
-    "context": [Document(page_content="affichage des score de la 13ieme journee de top14")]
+    "input": "Tu peux me donner des scores de match du top14?",
+    "context": [Document(page_content="affichage des scores de la 13e journee de top14")]
 })
 
 retriever = vector.as_retriever()
 retrieval_chain = create_retrieval_chain(retriever, document_chain)
 
-response = retrieval_chain.invoke({"input": "Tu peux me donner des score de match du top14?"})
-print(response["answer"])
+user_input = "Tu peux me donner des scores de match du top14?"
+response = retrieval_chain.invoke({"input": user_input})
+memory.add_response(response["answer"])
+
+user_input = "Tu peux me donner les meilleurs équipes du top14?"
+response = retrieval_chain.invoke({"input": user_input})
+memory.add_response(response["answer"])
+
+# Now you can use the previous responses in future interactions
+print("Previous Responses:", memory.get_previous_responses())
